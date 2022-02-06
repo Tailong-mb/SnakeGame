eb7260f3ea21f814c718203e109ab583da6390f5
Michael Bardou <michael.bardou@icloud.com>
2022-02-06 16:18:28 +0100

    add Image folder (contain img button)

---
+++ src/Menu.py
@@ -0,0 +1,41 @@
+import pygame
+import Button
+import SnakeGame
+
+SCREEN_WIDTH = 520
+SCREEN_HEIGHT = 520
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Snake Menu')
+
+#load image
+hard_button_img = pygame.image.load('../Image/hard_button.png').convert_alpha()
+normal_button_img = pygame.image.load('../Image/normal_button.png').convert_alpha()
+
+hard_button = Button.Button(140,300, normal_button_img)
+normal_button = Button.Button(140,150, hard_button_img)
+
+normal_game = False
+hard_game = False
+run = True
+while run:
+
+    screen.fill((202,228,241))
+
+    if hard_button.draw(screen):
+        normal_game = True
+        run = False
+    if normal_button.draw(screen):
+        hard_game = True
+        run = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+if normal_game:
+    SnakeGame.main_normal()
+if hard_game:
+    SnakeGame.main_hard()
+pygame.quit()
